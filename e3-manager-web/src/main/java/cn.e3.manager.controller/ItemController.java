99711b237e416ff220ad64bfa3823914dcb47cfa
wangpenghao <wphph_123456@126.com>
2018-08-19 10:27:41 +0800

    后台商品分页

--- e3-manager-web/src/main/java/cn.e3.manager.controller/ItemController.java
+++ e3-manager-web/src/main/java/cn.e3.manager.controller/ItemController.java
@@ -1,9 +1,11 @@
 package cn.e3.manager.controller;
 
+import cn.e3.utils.DatagridPageBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.e3.manager.service.ItemService;
@@ -30,6 +32,20 @@ public class ItemController {
 		TbItem item = itemService.findItemByID(itemId);
 			
 		return item;
+	}
+	/**
+	 * 需求:分页查询商品列表
+	 * 请求:/item/list
+	 * 参数:Integer page,Integer rows
+	 * 返回值:json格式DatagridPageBean
+	 */
+	@RequestMapping("/item/list")
+	@ResponseBody
+	public DatagridPageBean findItemList(@RequestParam(defaultValue="1")Integer page,
+										 @RequestParam(defaultValue="30") Integer rows){
+		//调用远程service服务对象方法,查询商品分页列表
+		DatagridPageBean pageBean = itemService.findItemList(page, rows);
+		return pageBean;
 	}
 
 }
