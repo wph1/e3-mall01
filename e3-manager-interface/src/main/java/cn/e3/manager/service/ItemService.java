264e7962a79f489934c72bd403ef41d52130cb9c
wangpenghao <wphph_123456@126.com>
2018-09-01 09:28:22 +0800

    文件上传与提交

--- e3-manager-interface/src/main/java/cn/e3/manager/service/ItemService.java
+++ e3-manager-interface/src/main/java/cn/e3/manager/service/ItemService.java
@@ -1,7 +1,9 @@
 package cn.e3.manager.service;
 
 import cn.e3.pojo.TbItem;
+import cn.e3.pojo.TbItemDesc;
 import cn.e3.utils.DatagridPageBean;
+import cn.e3.utils.E3mallResult;
 
 public interface ItemService {
 	
@@ -18,5 +20,13 @@ public interface ItemService {
 	 * 返回值:DatagridPageBean
 	 */
 	public DatagridPageBean findItemList(Integer page, Integer rows);
+
+	/**
+	 * 需求:保存商品表数据,商品描述表数据
+	 * 参数:TbItem item,TbItemDesc itemDesc
+	 * 返回值:E3mallResult
+	 */
+	public E3mallResult saveItem(TbItem item, TbItemDesc itemDesc);
+
 
 }
